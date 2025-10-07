9aecb2dbcb401ef0a96315c75db9dbf90d70a0fb
Mark Bailey <mcbaileygithub@gmail.com>
2025-10-07 11:59:55 -0400

    made some commits with nick

--- io.sh
+++ io.sh
@@ -8,4 +8,39 @@
 # -l fileleaf
 # -h has extension
 
+before="/c/users/mcbailey/repos/logics/keys.txt"
+
+# cat=$(ls $before)
+
+# echo $cat
+
+# echo $before
+
+# after=${before//i/kkkkkk}
+
+# echo $after
+
+# filename="example.tar.gz"
+
+# Extract the extension
+# extension="${filename##*.}"
+
+# echo "File extension: $extension"
+
+
+# get the file name
+echo $(basename "$before")
+
+
+# get the directory path
+echo $(basename $(dirname "$before"))
+
+
+# extension of file
+echo $()
+
+# gets the name of the directory 
+
+
+# echo $(dirname "$before")
 
