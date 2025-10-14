64915ba671b3e4a09ac096edfa313cb858f17d6c
Mark Bailey <mcbaileygithub@gmail.com>
2025-10-14 09:57:33 -0400

    created mappedrives.bat and postg.sh

---
+++ mapdrives.bat
@@ -0,0 +1,2 @@
+net use U: \\masterdesign1.toolingsystemsgroup.com\UGSystem
+net use G: \\masterdesign1.toolingsystemsgroup.com\Design
---
+++ postg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dir_name=$1
+directory="/G/CTS/junk/0Ufunc Testing/$dir_name"
+
+if ! [ -d "$directory" ]; then
+    mkdir "$directory"
+fi
+
+debugsigndotnet="/C/Users/mcbailey/repos/tsg-library/bin/DebugSignDotNet"
+cmp -s "$debugsigndotnet/tsg_library.dll" "$directory/tsg_library.dll"
+case $? in
+    0)
+        echo "the files haven't changed"
+    ;;
+    1)
+        cp "$debugsigndotnet/"*.dll "$directory"
+    ;;
+    2)
+    cp "$debugsigndotnet/"*.dll "$directory"
+    ;;
+    *)
+        echo "Unknown error code: $?"
+        exit 1
+    ;;
+esac
